c65a502f0325cdf226631d524db9470481bf19fd
Jayan Shrivastava <jshrivastava03@gmail.com>
2025-11-15 14:15:53 -0500

    remove vim with me and apm

--- lua/theprimeagen/lazy/local.lua
+++ lua/theprimeagen/lazy/local.lua
@@ -31,21 +31,6 @@ local local_plugins = {
             })
         end
     },
-    {
-        "vim-apm", dir = "~/personal/vim-apm",
-        config = function()
-            --[[
-            local apm = require("vim-apm")
-
-            apm:setup({})
-            vim.keymap.set("n", "<leader>apm", function() apm:toggle_monitor() end)
-            --]]
-        end
-    },
-    {
-        "vim-with-me", dir = "~/personal/vim-with-me",
-        config = function() end
-    },
 }
 
 return local_plugins
